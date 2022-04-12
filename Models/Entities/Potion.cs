b6488706f31269dee3b7d0683f09b401da30584e
bgabor88 <coindealerpro@gmail.com>
2022-04-12 06:38:20 +0200

    add potion class

---
+++ Models/Entities/Potion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogwartsPotions.Models.Entities
+{
+    public class Potion
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public Student Student { get; set; }
+
+        public IList<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    }
+}

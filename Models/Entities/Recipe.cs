08d9511c0e169b31109f91def42b028e6bcc0f2f
bgabor88 <coindealerpro@gmail.com>
2022-04-13 19:23:49 +0200

    add recipe model

---
+++ Models/Entities/Recipe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HogwartsPotions.Models.Entities;
+
+public class Recipe
+{
+    public Recipe()
+    {
+
+    }
+
+    public Recipe(Student student, HogwartsContext context, HashSet<Ingredient> ingredients)
+    {
+        Name = $"{student.Name} discovery #{context.CountUserRecipes(student)}";
+        Student = student;
+        Ingredients = ingredients;
+    }
+
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    public string Name { get; set; }
+
+
+    // Navigation properties
+    public Student Student { get; set; }
+    public HashSet<Ingredient> Ingredients { get; set; }
+}

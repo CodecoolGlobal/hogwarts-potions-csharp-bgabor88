e08d9c81dc7674f45b977e54f46398ed4d6faa4b
bgabor88 <coindealerpro@gmail.com>
2022-04-13 11:02:59 +0200

    implement student controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using HogwartsPotions.Models;
+using HogwartsPotions.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController, Route("/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly HogwartsContext _context;
+
+        public StudentController(HogwartsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Student>> GetAllStudents()
+        {
+            return await _context.GetAllStudent();
+        }
+
+        [HttpPost]
+        public async  Task AddStudent([FromBody] Student student)
+        {
+            await _context.AddStudent(student);
+            await _context.SaveChangesAsync();
+        }
+
+        [HttpGet("{id:long}")]
+        public async Task<Student> GetStudentById(long id)
+        {
+            return await _context.GetStudent(id);
+        }
+
+        [HttpPut("{id:long}")]
+        public async Task UpdateStudentById(long id, [FromBody] Student updatedStudent)
+        {
+            updatedStudent.Id = id;
+            await _context.UpdateStudent(updatedStudent);
+            await _context.SaveChangesAsync();
+        }
+
+        [HttpDelete("{id:long}")]
+        public async Task DeleteStudentById(long id)
+        {
+            await _context.DeleteStudent(id);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

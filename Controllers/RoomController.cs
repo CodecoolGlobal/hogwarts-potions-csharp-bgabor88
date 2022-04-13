2ce7eb698684288ed67b85a4cb90bfe01c94d3b5
bgabor88 <coindealerpro@gmail.com>
2022-04-13 13:25:53 +0200

    uncomment endpoint

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -52,10 +52,10 @@ namespace HogwartsPotions.Controllers
             await _context.SaveChangesAsync();
         }
 
-        //[HttpGet("rat-owners")]
-        //public async Task<List<Room>> GetRoomsForRatOwners()
-        //{
-        //    return await _context.GetRoomsForRatOwners();
-        //}
+        [HttpGet("rat-owners")]
+        public async Task<List<Room>> GetRoomsForRatOwners()
+        {
+            return await _context.GetRoomsForRatOwners();
+        }
     }
 }

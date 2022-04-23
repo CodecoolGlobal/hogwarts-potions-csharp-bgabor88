43dd216ce4090220bb3958946695e22a8eb7c74b
bgabor88 <coindealerpro@gmail.com>
2022-04-23 09:10:40 +0200

    remove unused files

--- HowartsFrontend/hogwarts-frontend/src/Components/PotionData.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function PotionData(props) {
-    const potions = props.potions;
-
-    return (
-        <div>test</div>
-    );
-}
--- HowartsFrontend/hogwarts-frontend/src/Utils/Utilities.js
+++
@@ -1,7 +0,0 @@
-export const stateUpdater = async (newElement, setter) => {
-  await setter((collection) =>
-    collection.map((element) => {
-      return element.id === newElement.id ? newElement : element;
-    })
-  );
-};

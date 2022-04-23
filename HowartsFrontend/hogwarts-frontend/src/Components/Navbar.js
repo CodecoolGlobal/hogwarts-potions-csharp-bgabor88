bced604a39ca6476722fc55fb76a824eee5df0ed
bgabor88 <coindealerpro@gmail.com>
2022-04-23 11:45:20 +0200

    add menu for user interactions

--- HowartsFrontend/hogwarts-frontend/src/Components/Navbar.js
+++ HowartsFrontend/hogwarts-frontend/src/Components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar as Navigation, Container, Nav, Dropdown, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar(props) {
   const Buttons = ["Rooms", "Students"];
@@ -47,6 +49,24 @@ function Navbar(props) {
           </Dropdown>
         </Nav>
       </Container>
+      <Dropdown align="end">
+        <Dropdown.Toggle className="arrowless-menu" variant="outline-warning" id="dropdown-basic">
+          <FontAwesomeIcon icon={faUser} />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item>
+            <button disabled type="button" className="navBtn mt-1 mb-1 btn btn-outline-warning">
+              Login
+            </button>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <button disabled type="button" className="navBtn mt-1 mb-1 btn btn-outline-warning">
+              Register
+            </button>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </Navigation>
   );
 }

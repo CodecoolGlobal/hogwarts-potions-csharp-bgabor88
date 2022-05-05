884edbdb8df244fe0b06291dd247fcb2bb628df3
bgabor88 <coindealerpro@gmail.com>
2022-05-05 08:06:56 +0200

    add potion repository interface

---
+++ Models/Interfaces/IPotionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+
+namespace HogwartsPotions.Models.Interfaces;
+
+public interface IPotionRepository
+{
+    public Task ModifyPotion(Potion potion, Ingredient ingredient);
+    public Task<List<Potion>> GetAllPotions();
+    public void AddPotion(Potion potion);
+    public Task<Potion> GetPotion(long potionId);
+    public Task DeletePotion(long id);
+}

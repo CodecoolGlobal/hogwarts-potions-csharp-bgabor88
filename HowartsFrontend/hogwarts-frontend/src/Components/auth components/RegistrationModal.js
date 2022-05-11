41c4e9b1150ac74e720f5e7f83e811baec1abc2e
bgabor88 <coindealerpro@gmail.com>
2022-05-11 10:50:28 +0200

    registration modal frame

---
+++ HowartsFrontend/hogwarts-frontend/src/Components/auth components/RegistrationModal.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { RegisterStudent } from "../../DAL/RegistrationComponents";
+
+export default function RegistrationModal(props) {
+  return (
+    <Modal show={props.show} onHide={() => props.setShow(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Registration</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <RegisterStudent setLogin={props.setLogin} setShow={props.setShow}/>
+      </Modal.Body>
+    </Modal>
+  );
+}

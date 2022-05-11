386d341060b9a1287788c6d2c47c81fa4922dcb6
bgabor88 <coindealerpro@gmail.com>
2022-05-11 17:19:39 +0200

    use fetchwrapper for recipes

--- HowartsFrontend/hogwarts-frontend/src/DAL/ContextProviders/RecipesContext.js
+++ HowartsFrontend/hogwarts-frontend/src/DAL/ContextProviders/RecipesContext.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect, createContext } from "react";
 import { apiGet } from "../CRUD";
+import { useFetchWrapper } from "../../_helpers/fetch-wrapper";
 
 export const RecipesContext = createContext();
 
 export const RecipesProvider = (props) => {
   const [recipes, setRecipes] = useState([]);
+  const fetchWrapper = useFetchWrapper();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await apiGet("/recipe");
+        const data = await fetchWrapper.get("/recipe");
         setRecipes([...data]);
       } catch (error) {
         console.log("error", error);

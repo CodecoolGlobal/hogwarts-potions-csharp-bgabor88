39163addd991d8e11bc5d6094172607531094f3f
bgabor88 <coindealerpro@gmail.com>
2022-05-11 15:33:39 +0200

    remove unused functions

--- HowartsFrontend/hogwarts-frontend/src/DAL/ContextProviders/RoomsContext.js
+++ HowartsFrontend/hogwarts-frontend/src/DAL/ContextProviders/RoomsContext.js
@@ -1,20 +1,5 @@
 import React, { useState, useEffect, createContext } from "react";
-import { apiDelete, apiGet, apiPost } from "../CRUD";
 import { useFetchWrapper } from "../../_helpers/fetch-wrapper";
-
-export const AddRoom = (setRooms, capacity) => {
-  console.log("here")
-  const fetchWrapper = useFetchWrapper();
-  const newRoom = fetchWrapper.post("/room", { capacity: capacity });
-  setRooms(rooms => [...rooms, newRoom]);
-};
-
-export const DeleteRoom = async (rooms, setRooms, id) => {
-  const fetchWrapper = useFetchWrapper();
-  const updatedRooms = [...rooms];
-  await fetchWrapper.delete(`room/${id}`);
-  await setRooms(updatedRooms.filter((room) => room.id !== id));
-};
 
 export const RoomsContext = createContext();
 
